0326263c70e0d3b64738a3985574edfcc2943405
heikoNickel <heiko.nickel@gmx.net>
2024-07-30 10:42:04 +0200

    Dummy Für Contacts

---
+++ script.js
@@ -0,0 +1,12 @@
+let contacts = [
+    {
+        "name"  : "Erika Krawotki",
+        "email" : "erika.krawotki@yahoo.de",
+        "phone" : "+491523659879",
+    },
+    {
+        "name"  : "Ralf Ralleisen",
+        "email" : "ralle.ralleisen@einsenralle.com",
+        "phone" : "+49178965123544",
+    }
+];
