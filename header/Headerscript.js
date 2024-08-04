d77d4efa943df9d571d4de4b28474330a06776b3
Marv92 <56001849+Edio1992@users.noreply.github.com>
2024-08-04 20:41:51 +0200

    Header als template
    Verschiedene neue scripte erstellt und eingebaut als Template ins main_contacts.html

---
+++ header/Headerscript.js
@@ -0,0 +1,18 @@
+export function loadHeader() {
+  const headerHTML = `
+        <header>
+            <div class="header-wrap">
+                <p>Kanban Project Management Tool</p>
+                <div class="align-this">
+                    <img src="../header/img_header/help.svg" alt="" />
+                    <div class="container">
+                    <img class="ellipse-style" src="../header/img_header/Ellipse_3.svg" alt="">                 
+                        <span class="span-size">SM</span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    `;
+
+  document.getElementById("header-placeholder").innerHTML = headerHTML;
+}
