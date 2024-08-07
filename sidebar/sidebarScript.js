180e3c0657c475ca46195aeeaacfc53289904882
Marv92 <56001849+Edio1992@users.noreply.github.com>
2024-08-07 10:37:46 +0200

    sidebar script+ Favicon
    sidebar script noch fehlerhaft+ Faviconhinzugefügt

--- sidebar/sidebarScript.js
+++ sidebar/sidebarScript.js
@@ -1,25 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-   
-    fetch('../sidebar/sidebar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.querySelector('.side-wrap').innerHTML = data;
-        addEventListeners();
-      })
-      .catch(error => console.error('Error loading sidebar:', error));
-    
-   
-    function addEventListeners() {
-      const buttons = document.querySelectorAll('.button-sidebar, .police-button');
-    
-      buttons.forEach(button => {
-        button.addEventListener('click', () => {
-          const targetPage = button.getAttribute('data-target');
-          if (targetPage) {
+  fetch('../sidebar/sidebar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector('.side-wrap').innerHTML = data;
+      addEventListeners(); 
+    })
+    .catch(error => console.error('Error loading sidebar:', error));
+
+  function addEventListeners() {
+    const buttons = document.querySelectorAll('.button-sidebar, .police-button');
+
+    buttons.forEach(button => {
+      button.addEventListener('click', (event) => {
+        event.preventDefault(); 
+        const targetPage = button.getAttribute('data-target');
+        if (targetPage) {
+          buttons.forEach(btn => btn.classList.remove('selected'));
+          button.classList.add('selected');
+
+          setTimeout(() => {
             window.location.href = targetPage;
-          }
-        });
+          }, 100);
+        }
       });
-    }
+    });
+
     
-  });
+    const summaryButton = document.getElementById('summary-button');
+    summaryButton.addEventListener('click', () => {
+      console.log('Summary button clicked!');
+    });
+  }
+});
