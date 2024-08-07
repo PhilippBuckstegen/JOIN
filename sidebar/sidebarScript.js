8f0285dfa0e26c44d7dfb8c9b45a0f97951f6702
Marv92 <56001849+Edio1992@users.noreply.github.com>
2024-08-07 12:16:24 +0200

    Sidebar hintergrund
    Sidebar hintergrund dunkel und erkennt welche seite aufgerufen wurde

--- sidebar/sidebarScript.js
+++ sidebar/sidebarScript.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(response => response.text())
     .then(data => {
       document.querySelector('.side-wrap').innerHTML = data;
-      addEventListeners(); 
+      addEventListeners(); // Füge Event-Listener hinzu, nachdem das HTML geladen wurde
+      highlightSelectedButton(); // Markiere den ausgewählten Button
     })
     .catch(error => console.error('Error loading sidebar:', error));
 
@@ -18,17 +19,24 @@ document.addEventListener('DOMContentLoaded', () => {
           buttons.forEach(btn => btn.classList.remove('selected'));
           button.classList.add('selected');
 
+          // Speichern der ID des ausgewählten Buttons im localStorage
+          localStorage.setItem('selectedButton', button.id);
+
           setTimeout(() => {
             window.location.href = targetPage;
           }, 100);
         }
       });
     });
+  }
 
-    
-    const summaryButton = document.getElementById('summary-button');
-    summaryButton.addEventListener('click', () => {
-      console.log('Summary button clicked!');
-    });
+  function highlightSelectedButton() {
+    const selectedButtonId = localStorage.getItem('selectedButton');
+    if (selectedButtonId) {
+      const selectedButton = document.getElementById(selectedButtonId);
+      if (selectedButton) {
+        selectedButton.classList.add('selected');
+      }
+    }
   }
 });
