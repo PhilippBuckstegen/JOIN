1ae0c6dfc294ddf12c05e832df72487917edb8c8
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-08-08 11:13:26 +0200

    responsiv board 320px

--- calendar/script.js
+++ calendar/script.js
@@ -2,17 +2,14 @@
  * Initializes the jQuery UI Datepicker and sets today's date in the input field.
  */
 $(function () {
-  // Initialisierung des Datepickers
   $("#datepicker").datepicker({
-    dateFormat: "dd.mm.yy", // Format des Datums
-    minDate: 0, // Verhindert die Auswahl von Daten in der Vergangenheit
+    dateFormat: "dd.mm.yy",
+    minDate: 0,
     onSelect: function (dateText) {
-      // Optional: Aktion beim Auswählen eines Datums
       console.log("Ausgewähltes Datum: " + dateText);
     },
   });
 
-  // Setze das heutige Datum als Wert des Eingabefelds
   const today = new Date();
   const day = String(today.getDate()).padStart(2, "0");
   const month = String(today.getMonth() + 1).padStart(2, "0");
@@ -21,58 +18,6 @@ $(function () {
 
   $("#datepicker").val(todayStr);
 })
-
-
-/* Alternative for Custom Month and Day Names */
-
-/**
- * Initializes the jQuery UI Datepicker, customizes month and day names, and sets today's date in the input field.
- *//*
-$(function () {
-  $("#datepicker").datepicker({
-    dateFormat: "dd.mm.yy", // Date format
-    minDate: 0, // Prevents selection of past dates
-    monthNames: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    dayNames: [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ],
-    onSelect: function (dateText) {
-      // Optional: Action when a date is selected
-      console.log("Selected Date: " + dateText);
-    },
-  });
-
-  // Set today's date as the value of the input field
-  const today = new Date();
-  const day = String(today.getDate()).padStart(2, "0");
-  const month = String(today.getMonth() + 1).padStart(2, "0");
-  const year = today.getFullYear();
-  const todayStr = `${day}.${month}.${year}`;
-
-  $("#datepicker").val(todayStr);
-}); */
-
-
-/* Older Versions */
 
 
 /**
