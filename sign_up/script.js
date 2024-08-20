5bc393923dd88b0bb83711b2d196cfce8e97d146
fabioltf <fabioltf@gmail.com>
2024-08-20 18:48:09 +0200

    Sign Up Design Anfang

--- sign_up/script.js
+++ sign_up/script.js
@@ -1,9 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const signUpButton = document.getElementById('signUp');
-  
-    // Event listener für den  sign-up button
-    signUpButton.addEventListener('click', function() {
-      const targetUrl = signUpButton.getAttribute('data-target');
-      window.location.href = targetUrl;
-    });
+const checkboxPrivacyPolicy = document.getElementById("checkboxPrivacyPolicy");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const signUpButton = document.getElementById("signUpBtn");
+
+  // Event listener für den  sign-up button
+  signUpButton.addEventListener("click", function () {
+    const targetUrl = signUpButton.getAttribute("data-target");
+    window.location.href = targetUrl;
   });
+});
+
+checkboxPrivacyPolicy.addEventListener("click", acceptPrivacyPolicy);
+
+function acceptPrivacyPolicy() {
+  if (!checkboxPrivacyPolicy.classList.contains("checkboxChecked")) {
+    checkboxPrivacyPolicy.classList.remove("checkboxUnchecked");
+    checkboxPrivacyPolicy.classList.add("checkboxChecked");
+    return 1;
+  } else {
+    checkboxPrivacyPolicy.classList.remove("checkboxChecked");
+    checkboxPrivacyPolicy.classList.add("checkboxUnchecked");
+    return 0;
+  }
+}
