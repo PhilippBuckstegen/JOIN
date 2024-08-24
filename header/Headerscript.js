dc30d6bd9de780252b959f49dd2158b3782a7fdb
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-08-24 18:59:33 +0200

    help site included

--- header/Headerscript.js
+++ header/Headerscript.js
@@ -17,13 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
               </div>
               <p class="p-header">Kanban Project Management Tool</p>
               <div class="align-this">
-                  <img src="../header/img_header/help.svg" alt="" />
+                  <img onclick="window.location.href='../help/help.html'" class="help-icon" src="../header/img_header/help.svg" alt="" />
                   <div onclick="toggleMenu()" class="container">
                       <img class="ellipse-style" src="../header/img_header/Ellipse_3.svg" alt="">
                       <span id="user-logged-in" class="span-size"></span>
                   </div>
                   <div id="slideInLogout" class="slide-in-logout d-none-logout slide-in-animation">
-                    <span class="p-slide-in-logout">Help</span>
+                    <span onclick="window.location.href='../help/help.html'" class="p-slide-in-logout">Help</span>
                     <span class="p-slide-in-logout">Legal Notice</span>
                     <span class="p-slide-in-logout">Privacy Policy</span>
                     <span id="logOut" class="p-slide-in-logout" onclick="logOut()">Log out</span>
