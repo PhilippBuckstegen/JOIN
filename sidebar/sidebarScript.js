976c69d9afc0661b7c3586ceec1638749a24b608
Edio1992 <56001849+Edio1992@users.noreply.github.com>
2024-08-26 12:46:39 +0200

    Korrektur der Sidebar verweist auch auf Elemente ausserhalb der Sidebar

--- sidebar/sidebarScript.js
+++ sidebar/sidebarScript.js
@@ -19,9 +19,30 @@ document.addEventListener('DOMContentLoaded', () => {
           buttons.forEach(btn => btn.classList.remove('selected'));
           button.classList.add('selected');
 
-        
+          // Ausgewählten Button in localStorage speichern
           localStorage.setItem('selectedButton', button.id);
 
+          setTimeout(() => {
+            window.location.href = targetPage;
+          }, 100);
+        }
+      });
+    });
+
+    // Event-Listener für die Elemente im Hauptbereich hinzufügen
+    const contentLinks = document.querySelectorAll('.sum-content-categories, .sum-content-boardtasks, .sum-content-deadlines');
+
+    contentLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+        const targetPage = link.getAttribute('data-target');
+        const correspondingButton = document.querySelector(`.button-sidebar[data-target='${targetPage}']`);
+        
+        if (correspondingButton) {
+          buttons.forEach(btn => btn.classList.remove('selected'));
+          correspondingButton.classList.add('selected');
+          localStorage.setItem('selectedButton', correspondingButton.id);
+
+          
           setTimeout(() => {
             window.location.href = targetPage;
           }, 100);
@@ -31,10 +52,28 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function highlightSelectedButton() {
+    const currentPath = window.location.pathname;
+    const buttons = document.querySelectorAll('.button-sidebar, .police-button');
+
+    buttons.forEach(button => {
+      const targetPage = button.getAttribute('data-target');
+
+      // Überprüfen, ob der aktuelle Pfad dem data-target entspricht
+      if (currentPath.endsWith(targetPage)) {
+        button.classList.add('selected');
+        console.log('Selected button:', button);
+        
+        // Ausgewählten Button in localStorage speichern
+        localStorage.setItem('selectedButton', button.id);
+      } else {
+        button.classList.remove('selected');
+      }
+    });
+
     const selectedButtonId = localStorage.getItem('selectedButton');
     if (selectedButtonId) {
       const selectedButton = document.getElementById(selectedButtonId);
-      if (selectedButton) {
+      if (selectedButton && !selectedButton.classList.contains('selected')) {
         selectedButton.classList.add('selected');
       }
     }
